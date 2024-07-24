06631a7b0f7b8eb16497f9cf35ee14d996b72abc
NatJerry <xiaomajunqwq123@outlook.com>
2024-07-24 16:06:03 +0800

    基本完成

--- src/main/kotlin/org/ncc/keepInventory/Main.kt
+++ src/main/kotlin/org/ncc/keepInventory/Main.kt
@@ -1,15 +1,13 @@
 package org.ncc.keepInventory
 
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import org.bukkit.Bukkit
-import org.bukkit.World
 import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.BufferedOutputStream
 import java.io.File
-import java.net.http.WebSocket.Listener
 import java.nio.charset.StandardCharsets
 
 class Main : JavaPlugin(), Listener {
@@ -28,10 +26,11 @@ class Main : JavaPlugin(), Listener {
         Bukkit.getWorlds().forEach { world ->
             run {
                 val file = File(dataFolder, "${world.name}.json")
-                var out = BufferedOutputStream(file.outputStream())
-                var worldSetting = WorldSetting(world.name, true, true)
+                val worldSetting = WorldSetting(world.name, true, true)
                 if (!file.exists()) {
                     if (!file.parentFile.exists()) file.parentFile.mkdirs()
+                    file.createNewFile()
+                    val out = BufferedOutputStream(file.outputStream())
                     out.use {
                         it.write(json.toJson(worldSetting).toByteArray(StandardCharsets.UTF_8))
                     }
@@ -40,6 +39,9 @@ class Main : JavaPlugin(), Listener {
             }
         }
         logger.info("worldSettingList loaded")
+        Bukkit.getPluginManager().registerEvents(this,this)
+        logger.info("Event Listener Registered")
+        logger.info("Enabled,to change WorldSetting,go to modify the json file of Each World and then restart the plugin")
 
     }
 
@@ -49,7 +51,22 @@ class Main : JavaPlugin(), Listener {
 
     @EventHandler
     fun onPlayerDeath(event: PlayerDeathEvent) {
-
+        val playerWorldName = event.player.world.name
+        worldSettingList.forEach { worldSetting ->
+            run {
+                if(worldSetting.worldName.equals(playerWorldName)){
+                    if(worldSetting.isKeepExp){
+                        event.keepLevel = true
+                        event.droppedExp = 0
+                    }
+                    if(worldSetting.isKeepInventory){
+                        event.keepInventory = true
+                        event.drops.clear()
+                    }
+                    return
+                }
+            }
+        }
     }
 
 }
